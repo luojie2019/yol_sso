5e2ebdf3bd92e150c727fd8ac3c7e9cbde8d5073
luojie2020 <luojie@yeezon.com>
2021-06-29 16:08:11 +0800

    0.0.2

--- lib/yol_sso/connections/message.rb
+++ lib/yol_sso/connections/message.rb
@@ -5,10 +5,19 @@ module YolSso
         http_post(send_url, body.merge("agentid": agentid))
       end
 
+      def deal_message(message_id)
+        body = {agentid: agentid, message_id: message_id}
+        http_post(deal_url, body.merge("agentid": agentid))
+      end
+
       private
 
       def send_url
         "#{host}messages"
+      end
+
+      def deal_url
+        "#{host}messages/deal"
       end
     end
   end
